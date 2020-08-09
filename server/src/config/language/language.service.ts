4461ab82e82a73d8a019516860b44e5705f35eb6
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-08-09 20:36:55 +0300

    Added config languages

---
+++ server/src/config/language/language.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@nestjs/common";
+import { LanguageEntity } from "./language.entity";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { GraphQLError } from "graphql";
+
+@Injectable()
+export class LanguageService {
+	constructor(
+		@InjectRepository(LanguageEntity)
+		private languageRepository: Repository<LanguageEntity>
+	) {}
+
+	async addLanguage(code: string) {
+		const language = await this.languageRepository.create({ code });
+
+		await this.languageRepository.save(language);
+
+		return language;
+	}
+
+	async setLanguageJson(id: number, jsonName: string) {
+		console.log(jsonName);
+
+		const language = await this.languageRepository.findOne({
+			where: { id }
+		});
+
+		if (!language) throw new GraphQLError("UNKNOWN_INFO_ID");
+
+		language.jsonName = jsonName;
+
+		await this.languageRepository.save(language);
+	}
+
+	async getlanguages() {
+		const languages = await this.languageRepository.find();
+
+		return languages;
+	}
+}

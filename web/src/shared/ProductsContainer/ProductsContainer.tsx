22756eedacd4306d3341ff939fa3358630ceb7ef
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-08-21 21:47:40 +0300

    Added ProcuctWrapper to ProductsContainer

--- web/src/shared/ProductsContainer/ProductsContainer.tsx
+++ web/src/shared/ProductsContainer/ProductsContainer.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ProductCard from "../ProductCard";
 import classes from "./ProductsContainer.module.scss";
+import ProductCardWrapper from "../../wrappers/ProductCardWrapper";
+import getProductCardWrapperTypes from "./utils/getProductCardWrapperTypes";
 
 interface Props {
 	cards: JSX.Element[];
 }
 
 const ProductsContainer = ({ cards }: Props) => {
-	const products = cards.filter(c => c && c.type === ProductCard);
+	const products = cards.filter(
+		c =>
+			c &&
+			(c.type === ProductCard ||
+				getProductCardWrapperTypes().includes(c.type))
+	);
 	return <div className={classes.productsContainer}>{products}</div>;
 };
 
